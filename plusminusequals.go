01a132a6c765c4d325ee75864bf5e7febb5f9b08
Evgeniy Sleptsov <slepcov@unitiki.com>
2024-03-06 13:52:04 +0300

    fixed realization / test cases

--- plusminusequals.go
+++ plusminusequals.go
@@ -1,10 +1,12 @@
 package esoterics
 
+import "strconv"
+
 // PlusMinusEquals is a simple esoteric programming language that uses only three commands: +, -, and =.
 // + increments the accumulator, - decrements the accumulator, and = outputs the accumulator as a character and a newline.
 func PlusMinusEquals(input string) string {
 	var output string
-	var accumulator byte
+	var accumulator int
 
 	for _, char := range input {
 		switch char {
@@ -13,7 +15,7 @@ func PlusMinusEquals(input string) string {
 		case '-':
 			accumulator--
 		case '=':
-			output += string(accumulator) + "\n"
+			output += strconv.Itoa(accumulator) + "\n"
 		}
 	}
 
